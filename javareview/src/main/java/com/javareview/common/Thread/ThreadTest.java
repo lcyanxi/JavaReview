26dd2e26b57d95534378da8162a3b888c950f216
lcyanxi <845892601@qq.com>
2017-07-22 18:24:11 +0800

    git-bug

--- javareview/src/main/java/com/javareview/common/Thread/ThreadTest.java
+++ javareview/src/main/java/com/javareview/common/Thread/ThreadTest.java
@@ -4,6 +4,13 @@ package com.javareview.common.Thread;
  * Created by lcyanxi on 2017/7/22.
  */
 public class ThreadTest {
+    /**
+     * git 提交在github上没记录  原因可能是用户名和邮箱跟github没有匹配上
+     * git config user.email 查看邮箱
+     * git config user.name 查看用户名
+     * 解决方案   删除从新fork一个
+     */
+
     public static void main(String args[]){
         for (int i=0;i<1000;i++){
             System.out.println(Thread.currentThread().getName()+"-------"+i);
